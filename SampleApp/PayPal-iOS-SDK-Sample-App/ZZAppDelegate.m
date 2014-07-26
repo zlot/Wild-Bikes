ce63fbb3d99dd4aee583f6d9d1086645d83b7254
Mark C Mitchell <mark@markcmitchell.net>
2014-07-26 22:33:08 +1000

    apparently added enough frameworks to make compiler happy for Parse SDK.

--- SampleApp/PayPal-iOS-SDK-Sample-App/ZZAppDelegate.m
+++ SampleApp/PayPal-iOS-SDK-Sample-App/ZZAppDelegate.m
@@ -8,11 +8,15 @@
 
 #import "ZZAppDelegate.h"
 #import "PayPalMobile.h"
+#import <Parse/Parse.h>
 
 @implementation ZZAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
+    [Parse setApplicationId:@"AxG1hPF80f8qyw9GR9L0l25nV2vArzX6aV7t32Se"
+                  clientKey:@"RUb3mhez4QHgc8SlRjAaQsFou3SoTYhzZ4h7LsbZ"];
+    
 #warning "Enter your credentials"
   [PayPalMobile initializeWithClientIdsForEnvironments:@{PayPalEnvironmentProduction : @"ASMhlxAMQU2G7pPdnVlJdvIsrfDVKHbvo9QlITudnHccHTRyMtMVfIDcwzyN",
                                                          PayPalEnvironmentSandbox : @"AWlyDRDv9N6I3TywZEAk0G5sZBZJPnZTCkTN5kRCiAVuNTeqVEoqxO2kwqp1"}];
