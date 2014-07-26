a8edfaeba85bd3ca4ac69301eea2e27bd596277c
Mark C Mitchell <mark@markcmitchell.net>
2014-07-27 04:04:52 +1000

    add assets

---
+++ SampleApp/PayPal-iOS-SDK-Sample-App/RidingModeViewController.h
@@ -0,0 +1,13 @@
+//
+//  RidingModeViewController.h
+//  WildBikesPaypalParse
+//
+//  Created by Mark C Mitchell on 27/07/2014.
+//  Copyright (c) 2014 WildBikes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RidingModeViewController : UIViewController
+
+@end
